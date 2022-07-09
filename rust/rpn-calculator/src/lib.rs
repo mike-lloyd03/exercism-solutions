52b3b1cadb9e03264d9244529c98bae0eae05ed2
Mike Lloyd <mike.lloyd03@pm.me>
2022-07-08 21:10:53 -0700

    Passed RPN tests

--- rust/rpn-calculator/src/lib.rs
+++ rust/rpn-calculator/src/lib.rs
@@ -9,25 +9,34 @@ pub enum CalculatorInput {
 
 pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
     let mut reg: Vec<i32> = Vec::new();
+
     for input in inputs {
         match *input {
             CalculatorInput::Value(v) => reg.push(v),
-            _ => reg = vec![math(&reg, input)?],
+            CalculatorInput::Add => {
+                let a1 = reg.pop()?;
+                let a2 = reg.pop()?;
+                reg.push(a2 + a1);
+            }
+            CalculatorInput::Subtract => {
+                let s1 = reg.pop()?;
+                let s2 = reg.pop()?;
+                reg.push(s2 - s1);
+            }
+            CalculatorInput::Multiply => {
+                let m1 = reg.pop()?;
+                let m2 = reg.pop()?;
+                reg.push(m2 * m1);
+            }
+            CalculatorInput::Divide => {
+                let d1 = reg.pop()?;
+                let d2 = reg.pop()?;
+                reg.push(d2 / d1);
+            }
         }
+    }
+    if reg.len() > 1 {
+        return None;
     }
     reg.pop()
-}
-
-fn math(reg: &[i32], op: &CalculatorInput) -> Option<i32> {
-    if reg.len() == 2 {
-        match *op {
-            CalculatorInput::Add => Some(reg[0] + reg[1]),
-            CalculatorInput::Subtract => Some(reg[0] - reg[1]),
-            CalculatorInput::Multiply => Some(reg[0] * reg[1]),
-            CalculatorInput::Divide => Some(reg[0] / reg[1]),
-            CalculatorInput::Value(_) => None,
-        }
-    } else {
-        None
-    }
 }
